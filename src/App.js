a5cb9943db86d377cfe995b08491fca6dd5321ed
fabiotano <fabio.capriotti20@gmail.com>
2023-10-25 10:40:19 -0300

    forst commit - components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './components/Card';
+import AddUser from './components/AddUser';
+
+
+function App() {
+  return (
+    <div>
+      <AddUser/>
+    </div>
+  );
+}
+
+export default App;
